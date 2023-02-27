5eb14e9846c893a8ec2e587fd3473246632d0056
pulkras <maligin128@gmail.com>
2023-02-27 23:57:00 +0300

    optinos and info about author

---
+++ src/game/Main.java
@@ -0,0 +1,48 @@
+package game;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int rows=4;
+    public static int columns=4;
+    public static int enemies=5;
+    public static int transistors=40;
+    public static int moves=20;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command;
+
+        do {
+            System.out.println("Welcome to JavaGame. This game is an example of usage java constructions.\n" +
+                    "Make your choice and press Enter, please\n" +
+                    "put 1 to start a new game\n" +
+                    "put 2 to see options\n" +
+                    "put 3 to see info about author\n" +
+                    "put 4 to exit from this game");
+
+            command = scanner.nextLine();
+
+            if (command.equals("1"))
+                startNewGame();
+            else if(command.equals("2"))
+                OptionsMenu.showOptions();
+            else if(command.equals("3"))
+                showInfoAboutAuthor();
+            else if(!command.equals("4"))
+                System.out.println("please try again to choose a number.");
+        } while(!command.equals("4"));
+    }
+
+    private static void startNewGame() {
+
+    }
+
+    private static void showInfoAboutAuthor() {
+        System.out.println("This game was created by pulkras.\n" +
+                "I took the idea from Дмитрий Финашкин channel.\n" +
+                "I decided to do this because I've never done games on Java previously\n");
+    }
+}
---
+++ src/game/OptionsMenu.java
@@ -0,0 +1,88 @@
+package game;
+
+import java.util.Scanner;
+
+public class OptionsMenu {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String command;
+    static String changes;
+
+    static String optionToChoose;
+
+    public static void showOptions() {
+        do {
+            System.out.println("make your choice and press Enter, please\n" +
+                    "put 1 to show your current settings\n" +
+                    "put 2 to change settings\n" +
+                    "put 3 to exit");
+            command = scanner.nextLine();
+
+            if(command.equals("1"))
+                showCurrentSettings();
+            else if(command.equals("2"))
+                changeSettings();
+            else if(!command.equals("3"))
+                System.out.println("try again to put correct number");
+
+
+        } while(!command.equals("3"));
+    }
+
+    public static void showCurrentSettings() {
+        System.out.println("Current settings:\n" +
+                "rows: " + Main.rows + "\n" +
+                "columns: " + Main.columns + "\n" +
+                "enemies: " + Main.enemies + "\n" +
+                "transistors: " + Main.transistors + "\n" +
+                "moves: " + Main.moves);
+    }
+
+    public static void changeSettings() {
+        System.out.println("Put 1 to change one option or 2 to change more");
+        changes = scanner.nextLine();
+        if(changes.equals("1")) {
+            changeOneOptionManually();
+        }
+        else if(changes.equals("2")) {
+            System.out.println("enter a new value for rows: ");
+            Main.rows = scanner.nextInt();
+            System.out.println("enter a new value for columns: ");
+            Main.columns = scanner.nextInt();
+            System.out.println("enter a new value for enemies: ");
+            Main.enemies = scanner.nextInt();
+            System.out.println("enter a new value for transistors: ");
+            Main.transistors = scanner.nextInt();
+            System.out.println("enter a new value for moves: ");
+            Main.moves = scanner.nextInt();
+        }
+    }
+    public static void changeOneOptionManually() {
+        System.out.println("what option do you want to change?\n" +
+                "put 1 to change amount of rows\n" +
+                "put 2 to change amount of columns\n" +
+                "put 3 to change amount of enemies\n" +
+                "put 4 to change amount of transistors\n" +
+                "or put 5 to change your moves");
+        optionToChoose = scanner.nextLine();
+        System.out.println("Write your number");
+        switch (optionToChoose) {
+            case "1":
+                Main.rows = scanner.nextInt();
+                break;
+            case "2":
+                Main.columns = scanner.nextInt();
+                break;
+            case "3":
+                Main.enemies = scanner.nextInt();
+                break;
+            case "4":
+                Main.transistors = scanner.nextInt();
+                break;
+            case "5":
+                Main.moves = scanner.nextInt();
+                break;
+
+        }
+    }
+}
